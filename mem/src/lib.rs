3749905e1e656c821ab656ceb91ce3aa9cbda780
ParzivalSec <lukas.vogl12@gmail.com>
2018-04-05 16:33:14 +0200

    Reexport the newly implemented modules

--- mem/src/lib.rs
+++ mem/src/lib.rs
@@ -1,4 +1,11 @@
+// Re-export utility modules for virtual memory allocations,
+// pointer operations (alignment, ...) and an intrusive linked list
 pub mod virtual_mem;
-pub mod allocators;
 pub mod pointer_util;
 pub mod freelist;
+
+// Re-export modules that are requires and used as the basis for 
+// the memory realm
+pub mod allocators;
+pub mod bounds_checker;
+pub mod memory_realm;
